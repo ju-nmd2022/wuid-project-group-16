3a45a9a64621c648879178b8e79de07023bcc251
EdvinHF <Hoed22rl@student.ju.se>
2022-12-06 19:39:24 +0100

    hamburgare produktsida

--- product-page.js
+++ product-page.js
@@ -1,17 +1,19 @@
 const hBtn = document.querySelector(".hamburger-btn");
-const slide = document.querySelector(".slide-menu");
-
+const slide = document.querySelector(".pslide-menu");
+const html = document.querySelector("html");
 let menuOpen = false;
 
 hBtn.addEventListener("click", () => {
   if (!menuOpen) {
     hBtn.classList.add("open");
     menuOpen = true;
-    slide.classList.add("open-slide");
+    slide.classList.add("popen-slide");
+    html.classList.add("html-noscroll");
     console.log("hi");
   } else {
     hBtn.classList.remove("open");
     menuOpen = false;
-    slide.classList.remove("open-slide");
+    slide.classList.remove("popen-slide");
+    html.classList.remove("html-noscroll");
   }
 });
