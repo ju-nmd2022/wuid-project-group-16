64cd4385dc752a95be2c7095caa59622a5ca9558
EdvinHF <Hoed22rl@student.ju.se>
2022-12-08 14:50:54 +0100

    hamburgare

--- shopping-cart.js
+++ shopping-cart.js
@@ -4,6 +4,10 @@ const removeButton2 = document.getElementById("removeButton2");
 const chocolateFlavoured = document.getElementById("chocolateFlavoured");
 const breakfastBlend = document.getElementById("breakfastBlend");
 
+const hBtn = document.querySelector(".hamburger-btn");
+const slide = document.querySelector(".pslide-menu");
+let menuOpen = false;
+
 removeButton1.addEventListener("click", () => {
   console.log("test");
   breakfastBlend.style.display = "none";
@@ -12,4 +16,17 @@ removeButton1.addEventListener("click", () => {
 removeButton2.addEventListener("click", function (event) {
   console.log("test");
   chocolateFlavoured.style.display = "none";
+});
+
+hBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    hBtn.classList.add("open");
+    menuOpen = true;
+    slide.classList.add("popen-slide");
+    console.log("hi");
+  } else {
+    hBtn.classList.remove("open");
+    menuOpen = false;
+    slide.classList.remove("popen-slide");
+  }
 });
